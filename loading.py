4ba7fdfa070a39b7d85fbacfd9d5ee105570226f
Coricos <merylldin@gmail.com>
2017-11-02 12:16:22 +0100

    Load, slice and preprocess raw signals for models

--- loading.py
+++ loading.py
@@ -7,8 +7,10 @@ from toolbox import *
 class Loader :
 
     # Initialization
-    def __init__(self) :
+    def __init__(self, max_jobs=multiprocessing.cpu_count()-1) :
 
+        # Cares about multiprocessing instances
+        self.njobs = max_jobs
         # Root paths
         self.raw_path = './Raw_Data'
         self.fea_path = './Fea_Data'
@@ -88,3 +90,106 @@ class Loader :
         self.description = lab
         # Memory efficiency
         del raw, lab
+
+    # Slice the signal accordingly to the time_window and overlap
+    def sliding_extraction(self) :
+
+        # Local function for slicing
+        def slice_signal(sig) :
+
+            if len(sig) < self.time_window : return []
+            else :
+                # Init variables
+                tme, srt, top = [], 0, len(sig)
+                # Prepares multiprocessing
+                while srt <= top - self.time_window :
+                    tme.append((srt, srt + self.time_window))
+                    srt += int(self.overlap_rto * self.time_window)
+                # Take care of the last slice
+                try : tme.append((top - self.time_window, top))
+                except : pass
+                # Launch multiprocessing
+                pol = multiprocessing.Pool(processes=min(len(tme), self.njobs))
+                mvs = pol.map(partial(extract, data=sig), tme)
+                pol.close()
+                pol.join()
+                # Memory efficiency
+                del tme, srt, top, pol
+                # Return the sliced signals
+                return mvs
+
+        # Where to gather the new arrays
+        X_tr, y_tr, X_va, y_va = [], [], [], []
+        # Deals with the training set
+        for ids in self.usr_train :
+            for exp in np.unique(self.description.query('User' == {}.format(ids))['Experience']) :
+                cut = self.description.query('Experience == {} & User == {}'.format(exp, ids))
+                for val in cut[['Label', 'Begin', 'End']].values :
+                    tmp = self.raw_signals.query('Experience == {} & User == {}'.format(exp, ids))
+                    sig = slice_signal(remove_columns(tmp, ['Experience', 'User']))
+                    y_tr += list(np.full(len(sig), cut[0]))
+                    X_tr += sig
+                    del tmp, sig
+                del cut
+        # Deals with the validation set
+        for ids in self.usr_valid :
+            for exp in np.unique(self.description.query('User' == {}.format(ids))['Experience']) :
+                cut = self.description.query('Experience == {} & User == {}'.format(exp, ids))
+                for val in cut[['Label', 'Begin', 'End']].values :
+                    tmp = self.raw_signals.query('Experience == {} & User == {}'.format(exp, ids))
+                    sig = slice_signal(remove_columns(tmp, ['Experience', 'User']))
+                    y_va += list(np.full(len(sig), cut[0]))
+                    X_va += sig
+                    del tmp, sig
+                del cut
+        # Save as attributes
+        self.X_tr = np.asarray(X_tr)
+        self.y_tr = np.asarray(y_tr).astype(int) - 1
+        self.X_va = np.asarray(X_va)
+        self.y_va = np.asarray(y_va).astype(int) - 1
+        # Memory efficiency
+        del X_tr, X_va, y_tr, y_va, self.raw_signals, self.description
+
+    # Preprocess the raw signals
+    def load_raw(self) :
+
+        # Prepares the data
+        self.load_signals()
+        self.sliding_extraction()
+
+        # Local function for processing
+        def process(img, scalers) :
+
+            # Save and apply new adapted format
+            sz0, sz1 = img.shape[0], img.shape[1]*img.shape[2]/8
+            img = np.asarray([img[:, sig, :] for sig in range(img.shape[1])])
+            img = img.reshape(img.shape[0], img.shape[1]*img.shape[2])
+            # Rescale the data
+            for idx in range(img.shape[0]) : 
+                if idx in [6, 7] : 
+                    img[idx, :] = scalers[idx].fit_transform(np.log(img[idx, :]).reshape(-1, 1)).reshape(img.shape[1])
+                else : 
+                    img[idx, :] = scalers[idx].fit_transform(img[idx, :].reshape(-1, 1)).reshape(img.shape[1])
+            # Reshape as entry
+            img = np.asarray([img[idx, :].reshape(sz0, sz1) for idx in range(img.shape[0])])
+            img = np.asarray([[img[idx, ind, :] for idx in range(img.shape[0])] for ind in range(img.shape[1])])
+            del sz0, sz1
+
+            return img
+
+        # Defines the scalers
+        sca = [Pipeline([('mms', MinMaxScaler(feature_range=(-1,1))), ('std', StandardScaler(with_std=False))]) for i in range(8)]
+        # Fit the scalers
+        img = np.concatenate((self.X_tr, self.X_va))
+        img = np.asarray([img[:, sig, :] for sig in range(img.shape[1])])
+        img = img.reshape(img.shape[0], img.shape[1]*img.shape[2])
+        for idx in range(img.shape[0]) : 
+            if idx in [6, 7] : lst[idx].fit(np.log(img[idx, :]).reshape(-1, 1))
+            else : lst[idx].fit(img[idx, :].reshape(-1, 1))
+        # Save a attributes the preprocessed versions
+        self.X_tr = process(self.X_tr, sca)
+        self.X_va = process(self.X_va, sca)
+        # Memory efficiency
+        del img, sca
+        # Return object
+        return self
