f849f8598fc832edc98d5a87a8015b58d64aeb1b
Coricos <merylldin@gmail.com>
2017-11-02 20:05:38 +0100

    Improved max_epochs

--- launcher.py
+++ launcher.py
@@ -5,18 +5,10 @@ from models import *
 
 if __name__ == '__main__' :
 
-	mod = Models('XGBoost').learn(n_iter=75, verbose=1)
-	mod.save_model()
-	del mod
+    mod = Models('Conv1D').learn(max_epochs=500, verbose=1)
+    mod.save_model()
+    del mod
 
-	mod = Models('RandomForest').learn(n_iter=75, verbose=1)
-	mod.save_model()
-	del mod
-
-	mod = Models('Conv1D').learn(verbose=1)
-	mod.save_model()
-	del mod
-
-	mod = Models('DeepConv1D').learn(verbose=1)
-	mod.save_model()
-	del mod
+    mod = Models('DeepConv1D').learn(max_epochs=500, verbose=1)
+    mod.save_model()
+    del mod
