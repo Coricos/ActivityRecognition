e63d48bb3b520e5b641e32e058aa446df447259c
Coricos <meryll.dindin@student.ecp.fr>
2017-11-03 10:20:34 +0100

    Reduced version

--- launcher.py
+++ launcher.py
@@ -11,12 +11,4 @@ if __name__ == '__main__' :
 
 	mod = Models('RandomForest').learn(n_iter=75, verbose=1)
 	mod.save_model()
-	del mod
-
-	mod = Models('Conv1D').learn(verbose=1)
-	mod.save_model()
-	del mod
-
-	mod = Models('DeepConv1D').learn(verbose=1)
-	mod.save_model()
-	del mod
+        del mod
