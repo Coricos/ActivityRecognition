0d043d990d3eafaa228d22b4761036056cbde960
mdindin <merylldin@gmail.com>
2017-11-10 22:44:30 +0100

    Enhanced launcher for multiple test

--- launcher.py
+++ launcher.py
@@ -5,11 +5,32 @@ from models import *
 
 if __name__ == '__main__' :
 
-    mod = Models('XGBoost').learn(n_iter=75, verbose=1)
+    mod = Models('XGBoost').learn(n_iter=75, verbose=0)
     mod.save_model()
+    print(mod.performance())
     del mod
 
-    mod = Models('RandomForest').learn(n_iter=75, verbose=1)
+    mod = Models('RandomForest').learn(n_iter=75, verbose=0)
     mod.save_model()
+    print(mod.performance())
     del mod
 
+    mod = Models('Conv1D').learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
+
+    mod = Models('Conv2D').learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
+
+    mod = Models('DeepConv1D').learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
+
+    mod = Models('DeepConv2D').learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
