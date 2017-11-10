bffef0c6a517fa9840181fcf4d41d5de1d17bd14
Coricos <merylldin@gmail.com>
2017-11-10 10:53:02 +0100

    Fixing conv1D

--- models.py
+++ models.py
@@ -98,17 +98,17 @@ class Models :
 
             def conv_input(inp) :
 
-                mod = Conv1D(100, 18)(inp)
+                mod = Conv1D(100, 70)(inp)
                 mod = BatchNormalization()(mod)
                 mod = Activation('relu')(mod)
                 mod = MaxPooling1D(pool_size=2)(mod)
                 mod = Dropout(0.30)(mod)
-                mod = Conv1D(50, 32)(mod)
+                mod = Conv1D(50, 25)(mod)
                 mod = BatchNormalization()(mod)
                 mod = Activation('relu')(mod)
                 mod = MaxPooling1D(pool_size=2)(mod)
                 mod = Dropout(0.30)(mod)
-                mod = Conv1D(25, 64)(mod)
+                mod = Conv1D(25, 10)(mod)
                 mod = BatchNormalization()(mod)
                 mod = Activation('relu')(mod)
                 mod = GlobalAveragePooling1D()(mod)
