279ed3728f76974e57eab7656ca981e65f2a1345
Coricos <merylldin@gmail.com>
2017-11-13 12:04:22 +0100

    Debug on LSTM

--- models.py
+++ models.py
@@ -425,7 +425,7 @@ class Models :
             return mod
 
         # Define the inputs
-        inp = [Input(shape=X_tr[0].shape) for num in range(len(X_tr))]
+        inp = [Input(shape=X_tr[0][0].shape) for num in range(len(X_tr))]
         # Build model
         model = Model(inputs=inp, outputs=[build_model(inp, len(np.unique(y_tr)))])
         # Compile and launch the model
