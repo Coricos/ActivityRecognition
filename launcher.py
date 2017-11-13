679d25f227b7c36aac88bd05953a0261893d51b7
Coricos <merylldin@gmail.com>
2017-11-13 16:48:07 +0100

    New pipeline

--- launcher.py
+++ launcher.py
@@ -2,32 +2,42 @@ from models import *
 
 if __name__ == '__main__' :
 
-    # mod = Models('XGBoost').learn(n_iter=75, verbose=0)
-    # mod.save_model()
-    # print(mod.performance())
-    # del mod
+    mod = Models('Conv1D', truncate=True).learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
 
-    # mod = Models('RandomForest').learn(n_iter=75, verbose=0)
-    # mod.save_model()
-    # print(mod.performance())
-    # del mod
+    mod = Models('Conv2D', truncate=True).learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
 
-    # mod = Models('Conv1D').learn(max_epochs=200, verbose=1)
-    # mod.save_model()
-    # print(mod.performance())
-    # del mod
+    mod = Models('LSTM', truncate=True).learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
 
-    # mod = Models('Conv2D').learn(max_epochs=200, verbose=1)
-    # mod.save_model()
-    # print(mod.performance())
-    # del mod
+    mod = Models('DeepConv1D', truncate=True).learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
 
-    # mod = Models('DeepConv1D').learn(max_epochs=200, verbose=1)
-    # mod.save_model()
-    # print(mod.performance())
-    # del mod
+    mod = Models('DeepConv2D', truncate=True).learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
 
-    mod = Models('DeepConv2D').learn(max_epochs=200, verbose=1)
+    mod = Models('DeepLSTM', truncate=True).learn(max_epochs=200, verbose=1)
+    mod.save_model()
+    print(mod.performance())
+    del mod
+
+    mod = Models('XGBoost', truncate=True).learn(n_iter=75, verbose=0)
+    mod.save_model()
+    print(mod.performance())
+    del mod
+
+    mod = Models('RandomForest', truncate=True).learn(n_iter=75, verbose=0)
     mod.save_model()
     print(mod.performance())
     del mod
