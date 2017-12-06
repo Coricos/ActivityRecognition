Fixing wrong normed rotation speed

loading.py
@@ -90,7 +90,7 @@ class Loader :
         raw = fast_concatenate(raw, axis=0)
         # Build the norms (referential independance)
         raw['Normed_A'] = np.sqrt(np.square(raw['Acc_x'].values) + np.square(raw['Acc_y']) + np.square(raw['Acc_z']))
-        raw['Normed_G'] = np.sqrt(np.square(raw['Acc_x'].values) + np.square(raw['Acc_y']) + np.square(raw['Acc_z']))
+        raw['Normed_G'] = np.sqrt(np.square(raw['Gyr_x'].values) + np.square(raw['Gyr_y']) + np.square(raw['Gyr_z']))
         # Build the labels
         lab = pd.read_csv('{}/labels.txt'.format(self.raw_path), sep='\n', delimiter=' ', header=None)
         lab.columns = ['Experience', 'User', 'Label', 'Begin', 'End']