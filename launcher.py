cef31adda7730864286a4de01c5704b8f98d2bc3
Coricos <meryll.dindin@student.ecp.fr>
2018-01-30 00:36:28 +0100

    Catching up

--- launcher.py
+++ launcher.py
@@ -8,24 +8,24 @@ from creator import *
 if __name__ == '__main__' :
 
     # Create all databases
-    for ana in ['Hips', 'Hand', 'Torso'] :
-        # Define the loader
-        loa = SHL_Loader('../data_huawei/Safe_Keeper/dtb_{}.h5'.format(ana), 250, 0.25, ana)
-        loa.load_signals()
-        loa.define_test()
-        del loa
+    # for ana in ['Hips', 'Hand', 'Torso'] :
+    #     # Define the loader
+    #     loa = SHL_Loader('../data_huawei/Safe_Keeper/dtb_{}.h5'.format(ana), 250, 0.25, ana)
+    #     loa.load_signals()
+    #     loa.define_test()
+    #     del loa
 
     # Standardize databases
     for ana in ['Hips', 'Hand', 'Torso'] :
         # Define the constructor
         inp = '../data_huawei/Safe_Keeper/dtb_{}.h5'.format(ana)
         out = '../data_huawei/dtb_{}.h5'.format(ana)
-        Constructor(inp, out).build_database(quaternions=False, standardize=True)
+        Constructor(inp, out).standardize()
 
     # Launch the learning tasks
     with open('arguments.pk', 'rb') as raw : args = pickle.load(raw)
     for ana in ['Hips', 'Hand', 'Torso'] :
         # Defines the model and make it learn
-        mod = DynamicModel('../data_huawei/dtb_{}.h5', args)
+        mod = DynamicModel('../data_huawei/dtb_{}.h5'.format(ana), args)
         mod.learn('../clfs_hapt/model_{}.h5'.format(ana), max_epochs=50)
         del mod
