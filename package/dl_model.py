8dbb69b60e1ef2970409b17a1a3a9062ee217e59
root <meryll.dindin@student.ecp.fr>
2018-06-18 11:02:00 +0900

    Fix on title of confusion matrix

--- package/dl_model.py
+++ package/dl_model.py
@@ -500,7 +500,7 @@ class DL_Model :
             pth = self.mod.split('/')[-1]
             acc = accuracy_score(true, prd)
             tle = '{} | {} | Accuracy: {:.2%}'
-            plt.title(tle.format(title, pth, acc, kap))
+            plt.title(tle.format(title, pth, acc))
             htp.yaxis.set_ticklabels(htp.yaxis.get_ticklabels(), 
                 rotation=0, ha='right', fontsize=12)
             htp.xaxis.set_ticklabels(htp.xaxis.get_ticklabels(), 
